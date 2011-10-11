383175198d2eb01ff22dafd3cc7148f62e73fccc
Rich Layte <rich.layte@gmail.com>
2011-10-11 08:46:10 +0000

    add setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'kickstart',
+    py_modules = ['kickstart'],
+    url = 'http://google.com',
+    version = '0.1',
+    description = 'project setup script',
+    author = 'Richard Layte',
+    author_email = 'rich.layte@gmail.com'
+)
