8eeaf8e4aa78c1100d6e078024a05978d4d415cf
Rich Layte <rich.layte@gmail.com>
2011-10-23 20:16:49 +0000

    remove long_description from setup

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name = 'kickstart',
     description = 'Framework agnostic setup script',
-    long_description = open('README.md').read(),
     include_package_data = True,
     packages = ['kickstarter'],
     scripts = ['kickstart.py'],
     install_requires = ['PyYAML'],
     url = 'https://github.com/rlayte/kickstart',
-    version = '0.293',
+    version = '0.294',
     author = 'Richard Layte',
     author_email = 'rich.layte@gmail.com'
 )
